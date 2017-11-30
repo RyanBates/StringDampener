13dc1a9ff6d9aa3eaa1a5cbda3e2e085115ae381
Ryan Bates <rbates214@gmail.com>
2017-11-29 18:00:48 -0600

    work for cloth later

---
+++ Assets/Particle.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+
+namespace HooksLaw
+{
+    public class Particle
+    {
+        public Vector3 force;
+        public Vector3 position;
+        public Vector3 acceleration;
+        public Vector3 velocity;
+
+        public float mass;
+
+        public Particle()
+        {
+            position = Vector3.zero;
+            acceleration = Vector3.zero;
+            velocity = Vector3.zero;
+            force = Vector3.zero;
+            mass = 5;
+        }
+
+        public Particle(Vector3 p, Vector3 v, float m)
+        {
+            position = p;
+            acceleration = Vector3.zero;
+            velocity = v;
+            force = Vector3.zero;
+            mass = m;
+        }
+
+        public void AddForce(Vector3 f)
+        {
+           force = f;
+        }
+        
+        // Update is called once per frame
+        public Vector3 Update(float deltaTime)
+        {
+            acceleration = force / mass;
+            velocity += acceleration * deltaTime;
+            position += velocity * deltaTime;
+
+            return position;
+        }
+    }
+
+    public class SpringDamper
+    {
+        Particle _P1, _P2;
+        float _Ks;
+        float _Lo;
+
+        public SpringDamper()
+        {
+
+        }
+
+        public SpringDamper(Particle p1, Particle p2, float springConstant, float restLength)
+        {
+            _P1 = p1;
+            _P2 = p2;
+            _Ks = springConstant;
+            _Lo = restLength;
+        }
+
+        public float Spring(float springConstant)
+        {
+            _Ks = springConstant;
+
+            return _Ks;
+        }
+
+        public float Rest(float restLength)
+        { 
+            _Lo = restLength;
+
+            return _Lo;
+        }
+
+        public Vector3 Span()
+        {
+            _P1.position = _P1.position / Spring(5);
+            _P1.position.y = Rest(5);
+
+            return _P1.position;           
+        }
+    }
+}
---
+++ Assets/particleBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleBehaviour : MonoBehaviour {
+
+    HooksLaw.Particle particle;
+    HooksLaw.SpringDamper spring;
+
+    public GameObject _particle;
+    public GameObject anchor;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anchor.transform.position = Vector3.zero;
+
+        particle = new HooksLaw.Particle();
+        spring = new HooksLaw.SpringDamper();
+        particle.AddForce(new Vector3(0,-9.807f, 0));
+        spring.Spring(10);
+        spring.Rest(5);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _particle.transform.position = particle.Update(Time.deltaTime);
+
+        
+	}
+}
